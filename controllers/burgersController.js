ac4cd149fd7bf6f65069acab7b21883ee06b17bf
Alex Korjeski <akorjeski@gmail.com>
2020-02-26 18:34:08 -0500

    updated burger-block-handlebars and changed name to burger_name so the names would show

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,12 +2,12 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,38 +16,34 @@ router.get("/", function(req, res) {
   });
 });
 
-// router.post("/api/cats", function(req, res) {
-//   cat.create([
-//     "name", "sleepy"
-//   ], [
-//     req.body.name, req.body.sleepy
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name"],[req.body.burger_name], function(result) {
+    // Send back the ID of the new burger
+    res.json({ id: result.insertId });
+  });
+});
 
-// router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// router.delete("/api/burgers/:id", function(req, res) {
 //   var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
-
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-// router.delete("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   cat.delete(condition, function(result) {
+//   burger.delete(condition, function(result) {
 //     if (result.affectedRows == 0) {
 //       // If no rows were changed, then the ID must not exist, so 404
 //       return res.status(404).end();
@@ -55,7 +51,6 @@ router.get("/", function(req, res) {
 //       res.status(200).end();
 //     }
 //   });
-// });
 
-// Export routes for server.js to use.
+///Export routes for server.js to use.
 module.exports = router;
