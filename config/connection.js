77c44129ae9523a412f36910b24153175148af49
Alex Korjeski <akorjeski@gmail.com>
2020-03-02 09:49:39 -0500

    dealing with heroku, made changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection;
+
 if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{  connection = mysql.createConnection({
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{  
+  var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
+  port: 3306,
   password: "rootroot",
   database: "burgers_db"
 
 
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
